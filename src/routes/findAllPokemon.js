4e23dcb66f89534ecf23e79346f64c970a7a7290
Julien2L <julien.lehoussine-labatut@epitech.eu>
2022-05-25 15:40:20 +0200

    end

--- src/routes/findAllPokemon.js
+++ src/routes/findAllPokemon.js
@@ -1,11 +1,30 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require('sequelize')
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
+    if(req.query.name) {
+      const name = req.query.name
+      return Pokemon.findAll({
+        where: {
+          name: {
+            [Op.eql] : name 
+          }
+        }})
+      .then(pokemons =>{
+        const message =  `Il y a ${pokemons.lenght} qui correspondent à votre recherche : ${name}.`
+        res.json({ message, data: pokemons})
+      })
+    } else {
     Pokemon.findAll()
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
       })
-  })
+      .catch(error => {
+        const message = "La liste des pokemons n'a pas pu être récupéré."
+        res.status(500).json({ message, data: error})
+      })
+  }
+})
 }  
